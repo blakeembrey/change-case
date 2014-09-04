3907563d68b18ce28c10d7dbfbb71b7dbb4cb47b
C. James Winslow <whoaa512@gmail.com>
2014-09-04 15:56:21 -0700

    no computation, just straight exports :ok::sheep:

--- change-case.js
+++ change-case.js
@@ -1,32 +1,19 @@
-var camel = require('camel-case');
-
 /**
- * Map of module names to tuples of short alias name and require definition.
- * Long alias names will be generated automatically based on the module name.
+ * Straight exports with short alias, to support browserify.
  *
- * @type {Object}
  */
-var ALIASES = {
-  'dot-case':         ['dot', require('dot-case')],
-  'swap-case':        ['swap', require('swap-case')],
-  'path-case':        ['path', require('path-case')],
-  'upper-case':       ['upper', require('upper-case')],
-  'lower-case':       ['lower', require('lower-case')],
-  'camel-case':       ['camel', require('camel-case')],
-  'snake-case':       ['snake', require('snake-case')],
-  'title-case':       ['title', require('title-case')],
-  'param-case':       ['param', require('param-case')],
-  'pascal-case':      ['pascal', require('pascal-case')],
-  'constant-case':    ['constant', require('constant-case')],
-  'sentence-case':    ['sentence', require('sentence-case')],
-  'is-upper-case':    ['isUpper', require('is-upper-case')],
-  'is-lower-case':    ['isLower', require('is-lower-case')],
-  'upper-case-first': ['ucFirst', require('upper-case-first')],
-};
-
-/**
- * Automatically alias all modules and short names.
- */
-Object.keys(ALIASES).forEach(function (name) {
-  module.exports[camel(name)] = module.exports[ALIASES[name][0]] = ALIASES[name][1];
-});
+exports.dot      = exports.dotCase        = require('dot-case')
+exports.swap     = exports.swapCase       = require('swap-case')
+exports.path     = exports.pathCase       = require('path-case')
+exports.upper    = exports.upperCase      = require('upper-case')
+exports.lower    = exports.lowerCase      = require('lower-case')
+exports.camel    = exports.camelCase      = require('camel-case')
+exports.snake    = exports.snakeCase      = require('snake-case')
+exports.title    = exports.titleCase      = require('title-case')
+exports.param    = exports.paramCase      = require('param-case')
+exports.pascal   = exports.pascalCase     = require('pascal-case')
+exports.constant = exports.constantCase   = require('constant-case')
+exports.sentence = exports.sentenceCase   = require('sentence-case')
+exports.isUpper  = exports.isUpperCase    = require('is-upper-case')
+exports.isLower  = exports.isLowerCase    = require('is-lower-case')
+exports.ucFirst  = exports.upperCaseFirst = require('upper-case-first')
