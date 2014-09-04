b2743c04bd0b3c8e7164ef7d1c6ecbe1559d0114
C. James Winslow <whoaa512@gmail.com>
2014-09-04 15:39:11 -0700

    refactor to work with browserify

--- change-case.js
+++ change-case.js
@@ -1,32 +1,32 @@
 var camel = require('camel-case');
 
 /**
- * Map of module names to short alias name. Long alias names will be generated
- * automatically based on the module name.
+ * Map of module names to tuples of short alias name and require definition.
+ * Long alias names will be generated automatically based on the module name.
  *
  * @type {Object}
  */
 var ALIASES = {
-  'dot-case':         'dot',
-  'swap-case':        'swap',
-  'path-case':        'path',
-  'upper-case':       'upper',
-  'lower-case':       'lower',
-  'camel-case':       'camel',
-  'snake-case':       'snake',
-  'title-case':       'title',
-  'param-case':       'param',
-  'pascal-case':      'pascal',
-  'constant-case':    'constant',
-  'sentence-case':    'sentence',
-  'is-upper-case':    'isUpper',
-  'is-lower-case':    'isLower',
-  'upper-case-first': 'ucFirst'
+  'dot-case':         ['dot', require('dot-case')],
+  'swap-case':        ['swap', require('swap-case')],
+  'path-case':        ['path', require('path-case')],
+  'upper-case':       ['upper', require('upper-case')],
+  'lower-case':       ['lower', require('lower-case')],
+  'camel-case':       ['camel', require('camel-case')],
+  'snake-case':       ['snake', require('snake-case')],
+  'title-case':       ['title', require('title-case')],
+  'param-case':       ['param', require('param-case')],
+  'pascal-case':      ['pascal', require('pascal-case')],
+  'constant-case':    ['constant', require('constant-case')],
+  'sentence-case':    ['sentence', require('sentence-case')],
+  'is-upper-case':    ['isUpper', require('is-upper-case')],
+  'is-lower-case':    ['isLower', require('is-lower-case')],
+  'upper-case-first': ['ucFirst', require('upper-case-first')],
 };
 
 /**
  * Automatically alias all modules and short names.
  */
 Object.keys(ALIASES).forEach(function (name) {
-  exports[camel(name)] = exports[ALIASES[name]] = require(name);
+  module.exports[camel(name)] = module.exports[ALIASES[name][0]] = ALIASES[name][1];
 });
