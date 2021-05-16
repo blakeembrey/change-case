b2f9fdbfa56877d68c76a1fcea18d2e1bb70174e
Ayush Gupta <aayush123@gmail.com>
2021-05-16 18:34:52 +0530

    ADDED Option to separate numbers in snake Case ADDED Test cases for new option

--- packages/snake-case/src/index.spec.ts
+++ packages/snake-case/src/index.spec.ts
@@ -1,6 +1,6 @@
-import { snakeCase } from ".";
+import { snakeCase, Options } from ".";
 
-const TEST_CASES: [string, string][] = [
+const TEST_CASES: [string, string, Options?][] = [
   ["", ""],
   ["_id", "id"],
   ["test", "test"],
@@ -9,12 +9,15 @@ const TEST_CASES: [string, string][] = [
   ["TestV2", "test_v2"],
   ["version 1.2.10", "version_1_2_10"],
   ["version 1.21.0", "version_1_21_0"],
+  ["TestV2", "test_v_2", { separateNumbers: true }],
+  ["address line 1", "address_line_1", { separateNumbers: true }],
+  ["Test string For 3rd Test", "test_string_for_3_rd_test", { separateNumbers: true }],
 ];
 
 describe("snake case", () => {
-  for (const [input, result] of TEST_CASES) {
+  for (const [input, result, options] of TEST_CASES) {
     it(`${input} -> ${result}`, () => {
-      expect(snakeCase(input)).toEqual(result);
+      expect(snakeCase(input, options)).toEqual(result);
     });
   }
 });
--- packages/snake-case/src/index.ts
+++ packages/snake-case/src/index.ts
@@ -1,10 +1,35 @@
-import { dotCase, Options } from "dot-case";
+import { dotCase, Options as DotCaseOptions } from "dot-case";
+
+type Options = DotCaseOptions & {
+  separateNumbers?: boolean;
+};
 
 export { Options };
 
-export function snakeCase(input: string, options: Options = {}) {
+function snakeCaseTransform(input: string, options: DotCaseOptions = {}) {
   return dotCase(input, {
     delimiter: "_",
     ...options,
   });
+}
+
+function snakeCaseTransformSplitNumbers(input: string, options: DotCaseOptions = {}) {
+  return dotCase(input, {
+    delimiter: "_",
+    splitRegexp: [
+      /([a-z0-9])([A-Z])/g,
+      /([A-Z])([A-Z][a-z])/g,
+      /([(0-9)])([A-Za-z])/g,
+      /([A-Za-z])([0-9])/g,
+    ],
+    ...options,
+  })
+}
+
+export function snakeCase(input: string, options: Options = {}) {
+  const { separateNumbers, ...rest } = options;
+  if (separateNumbers) {
+    return snakeCaseTransformSplitNumbers(input, rest);
+  }
+  return snakeCaseTransform(input, options);
 }
