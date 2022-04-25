3c9699abb8f416b4df56bfdac3e3ec9f4f256d9d
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-04-25 00:06:19 -0400

    add apaTitleCase folder and files, make some initial changes to index and tests

---
+++ packages/apa-case/src/index.ts
@@ -0,0 +1,37 @@
+const SMALL_WORDS = /\b(?:an?d?|a[st]|because|but|by|en|for|i[fn]|neither|nor|o[fnr]|only|over|per|so|some|tha[tn]|the|to|up|upon|vs?\.?|versus|via|when|with|without|yet)\b/i;
+const SMALL_WORDS_CAPITALIZED = /\b(s?he|it|we|him|her|you)\b/;
+const TOKENS = /[^\s:–—-]+|./g;
+const WHITESPACE = /\s/;
+const IS_MANUAL_CASE = /.(?=[A-Z]|\..)/;
+const ALPHANUMERIC_PATTERN = /[A-Za-z0-9\u00C0-\u00FF]/;
+
+export function apaTitleCase(input: string) {
+  let result = "";
+  let m: RegExpExecArray | null;
+
+  // tslint:disable-next-line
+  while ((m = TOKENS.exec(input)) !== null) {
+    const { 0: token, index } = m;
+
+    if (
+      // Ignore already capitalized words.
+      !IS_MANUAL_CASE.test(token) &&
+      // Ignore small words except at beginning or end.
+      (!(token.length < 4) ||
+        SMALL_WORDS_CAPITALIZED.test(token) ||
+        index === 0 ||
+        index + token.length === input.length) &&
+      // Ignore URLs.
+      (input.charAt(index + token.length) !== ":" ||
+        WHITESPACE.test(input.charAt(index + token.length + 1)))
+    ) {
+      // Find and uppercase first word character, skips over *modifiers*.
+      result += token.replace(ALPHANUMERIC_PATTERN, (m) => m.toUpperCase());
+      continue;
+    }
+
+    result += token;
+  }
+
+  return result;
+}
