78d01e7b098f7dc880f0c1863ee28cc7570faea0
Ben Briggs <ben.briggs@york.ac.uk>
2025-03-13 10:39:33 +0000

    Prefer describe.each
    This is so that we can test `Object.create(null)`. Currently,
    that fails with `TypeError: Cannot convert object to primitive value`

--- packages/change-case/src/keys.spec.ts
+++ packages/change-case/src/keys.spec.ts
@@ -2,74 +2,84 @@ import { describe, it, expect } from "vitest";
 import { camelCase } from "./keys";
 import * as changeCase from "./index.js";
 
-type TestCase = [
-  unknown,
-  number | undefined,
-  unknown,
-  (changeCase.Options | changeCase.PascalCaseOptions)?,
-];
+type TestCase<Input, Output> = {
+  input: Input;
+  output: Output;
+  depth?: number;
+  options?: changeCase.Options | changeCase.PascalCaseOptions;
+};
 
-const TEST_CASES: TestCase[] = [
-  [
-    {
+type TestCases = TestCase<unknown, unknown>[];
+
+describe.each([
+  {
+    input: {
       first_name: "bob",
       last_name: "the builder",
       credentials: [{ built_things: true }],
     },
-    Infinity,
-    {
+    depth: Infinity,
+    output: {
       firstName: "bob",
       lastName: "the builder",
       credentials: [{ builtThings: true }],
     },
-  ],
-  [
-    {
+  },
+  {
+    input: {
       first_name: "bob",
       price: 8.21,
       favoriteAnimals: ["red", "green", 3, null, 7],
     },
-    Infinity,
-    {
+    depth: Infinity,
+    output: {
       firstName: "bob",
       price: 8.21,
       favoriteAnimals: ["red", "green", 3, null, 7],
     },
-  ],
-  [
-    {
+  },
+  {
+    input: {
       TEST_KEY: {
         FOO_BAR: true,
       },
     },
-    undefined,
-    {
+    depth: undefined,
+    output: {
       testKey: {
         FOO_BAR: true,
       },
     },
-  ],
-  [{ TEST: true }, 0, { TEST: true }],
-  [null, 1, null],
-  [
-    {
+  },
+  {
+    input: { TEST: true },
+    depth: 0,
+    output: { TEST: true },
+  },
+  {
+    input: null,
+    depth: 1,
+    output: null,
+  },
+  {
+    input: {
       outer_property_1_2: "outer",
       an_array: [{ inner_property_3_4: true }],
     },
-    Infinity,
-    {
+    depth: Infinity,
+    output: {
       outerProperty12: "outer",
       anArray: [{ innerProperty34: true }],
     },
-    { mergeAmbiguousCharacters: true },
-  ],
-  [new Date(), 1, new Date()],
-];
-
-describe("change keys", () => {
-  for (const [input, depth, result, options] of TEST_CASES) {
-    it(`${input} -> ${result}`, () => {
-      expect(camelCase(input, depth, options)).toEqual(result);
-    });
-  }
+    options: { mergeAmbiguousCharacters: true },
+  },
+  {
+    input: new Date(),
+    depth: 1,
+    output: new Date(),
+  },
+] as TestCases)(`$input -> $result`, ({ input, output, depth, options }) => {
+  it("should output the correct result", () => {
+    expect(camelCase(input, depth, options)).toEqual(output);
+  });
 });
